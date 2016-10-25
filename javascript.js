6a2aa7ff7717db471e633d9ef2b7810dd0010545
VictornitraM <victor.martin@gmx.at>
2016-10-25 12:44:02 +0200

    Für Negm von Victor!

--- javascript.js
+++ javascript.js
@@ -2,6 +2,10 @@
  * Created by Mohamed on 27.09.2016.
  */
 /*jslint browser:true */
+var y = 2;
+var z = 3;
+console.log(y + z);
+
 var x = document.getElementById("momo");
  x.remove();
 console.log("änderung");
